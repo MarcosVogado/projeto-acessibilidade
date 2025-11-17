5d90914a41192d448cef14e913d031ee7c20deb9
Marcos Vogado <marcoshcvogado@gmail.com>
2025-11-17 16:58:51 -0300

    Add focus trap to modal for accessibility
    Integrated Angular CDK A11yModule and applied cdkTrapFocus to the modal dialog to improve accessibility by trapping keyboard focus within the modal when open.

--- src/app/pages/modal/modal.component.ts
+++ src/app/pages/modal/modal.component.ts
@@ -2,16 +2,19 @@ import {
   Component,
   EventEmitter,
   Input,
-  Output,
+  Output
 } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
+
+import { A11yModule } from '@angular/cdk/a11y';
 
 import { Livro } from '../../models/interfaces';
 
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, A11yModule],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.css'
 })
